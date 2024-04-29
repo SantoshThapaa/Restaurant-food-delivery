09ace9d45e92d2cda07bbe28830c89ae183a0e9b
SantoshThapaa <st708611@gmail.com>
2024-04-29 14:54:35 +0545

    third commit

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { pizzas } from '@/data';
 
 const CategoryPage = () => {
   return (
-    <div>CategoryPage</div>
-  )
-}
+    <div className='flex flex-wrap text-orange-600'>
+      {pizzas.map((item) => (
+        <Link className='w-full h-[60vh] border-r-2 border-b-2 border-orange-600 sm:w-1/2' href={`/product/${item.id}`} key={item.id}>
+          {/* IMAGE CONTAINER */}
+          {item.img && (
+            <div className='relative'>
+              <Image src={item.img} alt="" fill />
+            </div>
+          )}
+          {/* TEXT CONTAINER */}
+          <div>
+            <h1>{item.title}</h1>
+            <h2>{item.price}</h2>
+            <button> Add to Cart</button>
+          </div>
 
-export default CategoryPage
+        </Link>
+      ))}
+
+    </div>
+  );
+};
+
+export default CategoryPage;
--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,21 +1,23 @@
 import Image from 'next/image'
 import React from 'react'
+import CountDown from './CountDown'
 
 const Offer = () => {
   return (
-    <div className='bg-black h-screen flex flex-col md:flex-row'>
+    <div className="bg-black h-screen flex flex-col md:flex-row md:justify-between md:bg-[url('/offer1.png')] md:h-[90vh]">
       {/* Text Container  */}
-      <div className="flex-1 flex flex-col justify-center items-center text-center gap-8 p-6">
+      < div className="flex-1 flex flex-col justify-center items-center text-center gap-8 p-6" >
         <h1 className="text-white text-5xl font-bold xl:text-6xl">Combo Set of Delicious Burger and Cold Drinks with French Fries</h1>
-        <p className="text-white xl:text-xl">Indulge in the perfect pairing of Berger Coldrink and French Fries for an unparalleled taste experience. Picture a refreshing Berger Coldrink, with its crisp fizz and tantalizing flavor, complementing the savory crunch of perfectly golden French fries.</p>
+        <p className="text-white xl:text-xl">Indulge in the perfect pairing of Berger Coldrink and Golden French Fries for an unparalleled taste experience. </p>
+        <CountDown />
         <button className="bg-orange-600 text-white rounded-md py-3 px-6">Order Now</button>
       </div>
-        {/* Image Container  */}
-      <div className="flex-1 w-full relative">
-        <Image src="/offer.png" alt="" fill className="object-contain"/>
-      </div>
+      {/* Image Container  */}
+      < div className="flex-1 w-full relative md:h-full" >
+        <Image src="/offer.png" alt="" fill className="object-contain" />
+      </div >
     </div>
-  )
-}
+  );
+};
 
-export default Offer
+export default Offer;
